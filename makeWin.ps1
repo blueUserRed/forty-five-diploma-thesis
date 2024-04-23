9e503bf585481b833bdeb2a625e1ff222572a337
Marvin Kurka <8013@htl.rennweg.at>
2024-04-23 12:12:47 +0200

    formatting

--- makeWin.ps1
+++ makeWin.ps1
@@ -10,11 +10,12 @@ cp ../HTL3RLogoRGB.eps .
 cp -r ../text ./text
 cp -r ../images ./images
 
-xelatex "\def\noMintedCode{}\input{diplomarbeit}" -shell-escape -halt-on-error -output-driver='xdvipdfmx -z1' -no-pdf
+#xelatex "\def\noMintedCode{}\input{diplomarbeit}" -shell-escape -halt-on-error -output-driver='xdvipdfmx -z1' -no-pdf
+xelatex "\def\noMintedCode{}\input{diplomarbeit}" -shell-escape -halt-on-error -output-driver='xdvipdfmx' -no-pdf
 makeindex -c -q diplomarbeit.idx
 bibtex diplomarbeit
-xelatex "\def\noMintedCode{}\input{diplomarbeit}" -shell-escape -halt-on-error -output-driver='xdvipdfmx -z1' -no-pdf
-xelatex ./diplomarbeit.tex -shell-escape -halt-on-error -output-driver='xdvipdfmx -z1'
+xelatex "\def\noMintedCode{}\input{diplomarbeit}" -shell-escape -halt-on-error -output-driver='xdvipdfmx' -no-pdf
+xelatex ./diplomarbeit.tex -shell-escape -halt-on-error -output-driver='xdvipdfmx'
 cp ./diplomarbeit.pdf ../out
 
 cd ..
